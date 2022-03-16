59e119f140d004995c74988cd7776ba1eb7149fd
Martin Krupa <kelib99@gmail.com>
2022-03-16 08:09:17 +0100

    AircraftListState implementation

---
+++ app/src/main/java/link/krupa/martin/aircraftlist/presentation/aircraft_list/AircraftListState.kt
@@ -0,0 +1,9 @@
+package link.krupa.martin.aircraftlist.presentation.aircraft_list
+
+import link.krupa.martin.aircraftlist.domain.model.Aircraft
+
+data class AircraftListState(
+    val isLoading : Boolean = false,
+    val aircraftList : List<Aircraft> = emptyList(),
+    val error : String = ""
+)
